88a1e9f3c57d210135ebed242c849b05234bcdf0
EZDEEN EQSOSI <53225302+ezoo96@users.noreply.github.com>
2023-10-17 01:39:58 +0300

    Update Messages.cs

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,21 +1,21 @@
-﻿namespace Business.Constants
+namespace Business.Constants
 {
     public static class Messages
     {
-        public static string Error = "Başarısız";
-        public static string Added = "Ekleme Başarılı";
-        public static string Listed = "Listeleme Başarılı";
-        public static string Deleted = "Silme Başarılı";
-        public static string Updated = "Güncelleme Başarılı";
-        public static string InvalidRental = " Kiralanacak araba müsait değil";
-        public static string UserNameInvalid = "Kullanıcı ismi geçersiz";
-        public static string ItemAdded = "Fotograf eklendi";
-        public static string AuthorizationDenied = "Yetkiniz yok";
-        public static string UserRegistered = "Kayıt oldu";
-        public static string UserNotFound = "Kullanıcı bulunamadı";
-        public static string PasswordError = "Paralo hatası";
-        public static string SuccessfulLogin = "Başarılı giriş";
-        public static string UserAlreadyExists = "Kullanıcı mevcut";
-        public static string AccessTokenCreated = "Token oluşturulur";
+        public static string Error = "Unsuccessful";
+        public static string Added = "Addition Successful";
+        public static string Listed = "Listing Successful";
+        public static string Deleted = "Delete Successful";
+        public static string Updated = "Update Successful";
+        public static string InvalidRental = " The car to rent is not available";
+        public static string UserNameInvalid = "Username is invalid";
+        public static string ItemAdded = "Photo added";
+        public static string AuthorizationDenied = "You have no authority";
+        public static string UserRegistered = "registered";
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "password error";
+        public static string SuccessfulLogin = "Successful login";
+        public static string UserAlreadyExists = "User available";
+        public static string AccessTokenCreated = "Token is created";
     }
 }
